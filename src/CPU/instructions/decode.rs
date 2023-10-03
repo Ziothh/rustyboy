a1e8e04ef1888eafd85c7b02fd1e7e04f6d177c2
Zioth <louisgiet.w@gmail.com>
2023-10-03 23:52:28 +0200

    Implemented the load operations

--- src/CPU/instructions/decode.rs
+++ src/CPU/instructions/decode.rs
@@ -1,6 +1,7 @@
 use std::ops::Index;
 
 use crate::{
+    prelude::NibbleFrom16bit,
     program::Program,
     CPU::memory::{Reg16, Reg8, Registers},
 };
@@ -12,6 +13,8 @@ pub enum Instruction {
     JP(JumpTest),
     /// Load values from memory
     LD(LoadType),
+    /// Load halfword
+    LDH(LoadHalfwordType),
 
     // [Function calls]
     /// Call a function
@@ -66,35 +69,54 @@ impl Instruction {
     pub fn try_from_opcode_default(byte: u8, program: &mut Program) -> Result<Self, String> {
         match byte {
             // [8-bit load instructions]
+            // LD r, r’: Load register (register)
             0x41 => Ok(Instruction::LD(
                 LoadType::Register { destination: program.next_byte().try_into()?, source: program.next_byte().try_into()?, }
             )),
+            // LD r, n: Load register (immediate)
             0x06 => Ok(Instruction::LD(
                 LoadType::Immediate { destination: program.next_byte().try_into()? }
             )),
+            // LD r, (HL): Load register (indirect HL)
             0x46 => Ok(Instruction::LD(
                 LoadType::FromIndirect { destination: program.next_byte().try_into()?, source: Reg16::HL }
             )),
+            // LD (HL), r: Load from register (indirect HL)
             0x70 => Ok(Instruction::LD(
                 LoadType::ToIndirect { source: program.next_byte().try_into()?, destination: Reg16::HL }
             )),
+            // LD (HL), n: Load from immediate data (indirect HL)
             0x36 => Ok(Instruction::LD(
                 LoadType::ImmediateToIndirect
             )),
+            // LD A, (BC): Load accumulator (indirect BC)
             0x0A => Ok(Instruction::LD(
                 LoadType::FromIndirect { destination: Reg8::A, source: Reg16::BC }
             )),
+            // LD A, (DE): Load accumulator (indirect DE)
             0x1A => Ok(Instruction::LD(
                 LoadType::FromIndirect { destination: Reg8::A, source: Reg16::DE }
             )),
+            // LD (BC), A: Load from accumulator (indirect BC)
             0x02 => Ok(Instruction::LD(
                 LoadType::ToIndirect { source: Reg8::A, destination: Reg16::BC }
             )),
+            // LD (DE), A: Load from accumulator (indirect DE)
             0x12 => Ok(Instruction::LD(
                 LoadType::ToIndirect { source: Reg8::A, destination: Reg16::DE }
             )),
+            // LD A, (nn): Load accumulator (direct)
             0xFA => Ok(Instruction::LD(
-                LoadType::FromIndirect { destination: Reg8::A, source:  }
+                LoadType::FromDirect { destination: Reg8::A, source: u16::from_nibbles(*program.next_byte(), *program.next_byte()) } 
+            )),
+            // LD (nn), A: Load from accumulator (direct)
+            0xEA => Ok(Instruction::LD(
+                LoadType::ToDirect { destination: u16::from_nibbles(*program.next_byte(), *program.next_byte()), source: Reg8::A } 
+            )),
+            // LDH A, (C): Load accumulator (indirect 0xFF00+C)
+            0xF2 => Ok(Instruction::LDH(
+                    LoadHalfwordType::FromIndirect { source: Reg8::A, destination: Reg8::C }
+
             )),
 
 
@@ -114,19 +136,6 @@ impl Instruction {
     }
 }
 
-// #[rustfmt::skip]
-// pub enum LoadByteTarget {
-//     A, B, C, D, E, H, L, HLI
-// }
-//
-// #[rustfmt::skip]
-// pub enum LoadByteSource {
-//     A, B, C, D, E, H, L,
-//     /// Direct 8bit value
-//     D8,
-//     /// Indicates that we use the HL virtual u16 as a load address
-//     HLI
-// }
 pub enum LoadType {
     /// Load the bytes from the `source` register into the `destination` register.
     ///
@@ -160,12 +169,8 @@ pub enum LoadType {
     /// ```
     FromIndirect {
         /// The register receiving the value.
-        ///
-        /// Read from memory
         destination: Reg8,
         /// The combined register containing a pointer to the memory addres.
-        ///
-        /// Implicit, based on opcode
         source: Reg16,
     },
 
@@ -177,14 +182,10 @@ pub enum LoadType {
     /// write(HL, B)
     /// ```
     ToIndirect {
-        /// The register containing the value.
-        ///
-        /// Read from memory
-        source: Reg8,
         /// The combined register containing a pointer to the memory addres.
-        ///
-        /// Implicit, based on opcode
         destination: Reg16,
+        /// The register containing the value.
+        source: Reg8,
     },
 
     /// Load the bytes from `destination` register into the memory address pointed to by the `HL` register.
@@ -195,12 +196,64 @@ pub enum LoadType {
     /// write(HL, n)
     /// ```
     ImmediateToIndirect,
+
+    /// Load to the 8-bit register, data from the absolute address specified by the 16-bit operand nn
+    ///
+    /// # Pseudo code
+    /// ```ignore
+    /// nn = unsigned_16(lsb=read(PC++), msb=read(PC++))
+    /// A = read(nn)
+    /// ```
+    FromDirect {
+        /// The 8-bit register that the data gets loaded into
+        destination: Reg8,
+        /// The memory address of the data that needs to be written to the destination
+        source: u16,
+    },
+
+    /// Load to the absolute address specified by the 16-bit operand nn, data from the 8-bit register.    
+    /// 
+    /// # Pseudo code
+    /// ```ignore
+    /// nn = unsigned_16(lsb=read(PC++), msb=read(PC++))
+    /// write(nn, A)
+    /// ```
+    ToDirect {
+        /// The memory address where the data gets loaded into
+        destination: u16,
+        /// The 8-bit register with the memory address of the data
+        source: Reg8,
+    },
+
+
     // Word: just like the Byte type except with 16-bit values
     // AFromIndirect: load the A register with the contents from a value from a memory location whose address is stored in some location
     // IndirectFromA: load a memory location whose address is stored in some location with the contents of the A register
     // AFromByteAddress: Just like AFromIndirect except the memory address is some address in the very last byte of memory.
     // ByteAddressFromA: Just like IndirectFromA except the memory address is some address in the very last byte of memory.
 }
+
+pub enum LoadHalfwordType {
+    /// Load to the 8-bit `source` register, data from the address specified by the 8-bit `destination` register. The full 16-bit absolute
+    /// address is obtained by setting the most significant byte to 0xFF and the least significant byte to the value of `destination`,
+    /// so the possible range is `0xFF00..=0xFFFF`.
+    ///
+    /// # Pseudo code
+    /// ```ignore
+    /// A = read(unsigned_16(lsb=C, msb=0xFF))
+    /// ```
+    FromIndirect {
+        /// The register containing the lsb of the memory address
+        source: Reg8,
+        /// The register the data gets loaded into
+        destination: Reg8,
+    }
+}
+
+
+
+
+
 pub enum IncDecTarget {
     // TODO: add other targets
     BC,
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,13 +1,15 @@
 // This file extends the std library
 
-pub trait NibbleFrom16bit<T> {
+pub trait NibbleFrom16bit<T, U> {
     fn top_nibble(&self) -> T;
     fn bottom_nibble(&self) -> T;
 
-    fn from_nibbles(top: T, bottom: T) -> Self;
+    /// @param `lsb` the least significant byte
+    /// @param `msb` the most significant byte
+    fn from_nibbles(lsb: T, msb: T) -> U;
 }
 
-impl NibbleFrom16bit<u8> for u16 {
+impl NibbleFrom16bit<u8, u16> for u16 {
     fn top_nibble(&self) -> u8 {
         ((self & 0xFF00) >> 8) as u8
     }
@@ -16,11 +18,11 @@ impl NibbleFrom16bit<u8> for u16 {
         (self & 0xFF) as u8
     }
 
-    fn from_nibbles(top: u8, bottom: u8) -> Self {
-        ((top as u16) << 8) | (bottom as u16)
+    fn from_nibbles(lsb: u8, msb: u8) -> u16 {
+        ((msb as u16) << 8) | (lsb as u16)
     }
 }
-impl NibbleFrom16bit<u8> for &u16 {
+impl NibbleFrom16bit<u8, u16> for &u16 {
     fn top_nibble(&self) -> u8 {
         ((*self & 0xFF00) >> 8) as u8
     }
@@ -29,7 +31,7 @@ impl NibbleFrom16bit<u8> for &u16 {
         (*self & 0xFF) as u8
     }
 
-    fn from_nibbles(top: u8, bottom: u8) -> Self {
-        &u16::from_nibbles(top, bottom)
+    fn from_nibbles(lsb: u8, msb: u8) -> u16 {
+        u16::from_nibbles(lsb, msb)
     }
 }
